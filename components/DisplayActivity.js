bc5107dd589f57f40511da12e36cddbe730587ae
Sisi Small <smallsianah@gmail.com>
2023-08-10 16:11:57 -0400

    added some more styling choices

--- components/DisplayActivity.js
+++ components/DisplayActivity.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Button, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 const DisplayActivity = ({ activitiesList }) => {
     const [selectedActivity, setSelectedActivity] = useState(null);
@@ -13,27 +13,71 @@ const DisplayActivity = ({ activitiesList }) => {
 
     return (
         <View style={styles.container}>
-            <Button title="What should I do today?" onPress={handleRandomSelection} />
-            {selectedActivity && <Text style={styles.activityText}>{selectedActivity}</Text>}
+            <View style={styles.box}>
+                <TouchableOpacity style={styles.button} onPress={handleRandomSelection}>
+                    <Text style={styles.buttonText}>What should I do today?</Text>
+                </TouchableOpacity>
+                {selectedActivity ? (
+                    <Text style={styles.activityText}>{selectedActivity}</Text>
+                ) : (
+                    <Text style={styles.instructionText}>Press the button to get a suggestion</Text>
+                )}
+            </View>
         </View>
     );
+    
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,                  // This makes sure the container takes the full height
-        justifyContent: 'center', // Align children vertically in the center
-        alignItems: 'center'      // Align children horizontally in the center
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+        backgroundColor: '#F6F6F9',
+    },
+    instructionText: {
+        fontSize: 18,
+        color: '#777',
+        marginTop: 20,
+        textAlign: 'center'
     },
     activityText: {
-        fontSize: 24,             // Increase font size
-        fontWeight: 'bold',       // Make it bold
-        marginTop: 20,            // Give some space from the button
-        textAlign: 'center'      // Center the text if it's multiline
-    }
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 20,
+        textAlign: 'center'
+    },
+    button: {
+        paddingVertical: 12,
+        paddingHorizontal: 25,
+        backgroundColor: '#007BFF',
+        borderRadius: 30,
+        elevation: 5,
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+    },
+    buttonText: {
+        fontSize: 18,
+        color: 'white',
+        fontWeight: '600',
+    },
+    box: {
+        padding: 20,
+        borderWidth: 2,
+        borderColor: '#007BFF',
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%'
+    },
+    
 });
 
 export default DisplayActivity;
+
+
 
 
 
