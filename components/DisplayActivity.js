cc5d75a33bf4af52e564415cb08f74a80818005c
Sisi Small <smallsianah@gmail.com>
2023-08-10 16:02:53 -0400

    playing around with styling now w/ functionality

--- components/DisplayActivity.js
+++ components/DisplayActivity.js
@@ -1,9 +1,7 @@
-import React, { useState, useContext } from 'react';
-import { View, Button, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Button, Text, StyleSheet } from 'react-native';
 
-
-const DisplayActivity = ({ activitiesList, setActivitiesList }) => {
-    
+const DisplayActivity = ({ activitiesList }) => {
     const [selectedActivity, setSelectedActivity] = useState(null);
 
     const handleRandomSelection = () => {
@@ -14,14 +12,29 @@ const DisplayActivity = ({ activitiesList, setActivitiesList }) => {
     };
 
     return (
-        <View>
+        <View style={styles.container}>
             <Button title="What should I do today?" onPress={handleRandomSelection} />
-            {selectedActivity && <Text>{selectedActivity}</Text>}
+            {selectedActivity && <Text style={styles.activityText}>{selectedActivity}</Text>}
         </View>
     );
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,                  // This makes sure the container takes the full height
+        justifyContent: 'center', // Align children vertically in the center
+        alignItems: 'center'      // Align children horizontally in the center
+    },
+    activityText: {
+        fontSize: 24,             // Increase font size
+        fontWeight: 'bold',       // Make it bold
+        marginTop: 20,            // Give some space from the button
+        textAlign: 'center'      // Center the text if it's multiline
+    }
+});
+
 export default DisplayActivity;
+
 
 
 
